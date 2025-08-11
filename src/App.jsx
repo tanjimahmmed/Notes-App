ca25f7b4a499bae6cc9f019805792caf51c0ebb9
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-11 12:37:07 +0600

    Add input fields

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NoteForm from "./components/NoteForm"
+
+const App = () => {
+  return (
+    <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 shadow-lg'>
+      <h2 className='text-2xl font-bold mb-4 text-center'>📝 Notes App</h2>
+      <NoteForm/>
+    </div>
+  )
+}
+
+export default App
