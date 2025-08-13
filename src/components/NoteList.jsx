9d81fbd6d526adfcc9d0df65de0731b0e63f6a7c
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-13 12:42:56 +0600

    add conditional rendering on noteList

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NoteList = ({notes}) => {
+const NoteList = ({notes, deleteNote}) => {
     if(notes.length === 0){
         return (
             <p className='text-center text-gray-500'>
@@ -12,13 +12,22 @@ const NoteList = ({notes}) => {
     return (
         <div className='space-y-4'>
             {notes.map((note) => (
-                <div key={note.id} className='p-4 bg-white rounded-lg shadow-md border-l-4'>
+                <div key={note.id} className='p-4 bg-white rounded-lg shadow-md border-l-4' 
+                style={{
+                    borderLeftColor:
+                    note.priority === 'High' ? 'red' : note.priority === 'Medium' ? 'orange' : 'green'
+                }}
+                >
                     <h3 className='text-lg font-bold'>{note.title}</h3>
                     <p className='text-sm text-gray-600'><strong>Category: </strong> {note.category}</p>
                     <p className='text-sm text-gray-600'><strong>Priority: </strong> {note.priority}</p>
                     <p className='mt-2'>
                         {note.description}
                     </p>
+
+                    <button onClick={() => deleteNote(note.id)} className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'>
+                        🗑️ Delete
+                    </button>
                 </div>
             ))}
         </div>
