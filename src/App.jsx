16c35894505e9c858f68b47c459cbea7d0e17580
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-14 20:38:44 +0600

    add localstorage side effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NoteForm from "./components/NoteForm"
 import NoteList from "./components/NoteList";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    return notes || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
 
   const deleteNote = (id) => {
 
